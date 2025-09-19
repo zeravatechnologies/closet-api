424c22c55bc1a60b0f83193acd80f8d6a8cab3a7
zeravatechnologies <zerava.technologies@gmail.com>
2025-09-19 11:19:07 -0400

    cohere: initial commit of actual project code (#1)
    Co-authored-by: Dhiraj Sah <ishantsah@gmail.com>

---
+++ src/main/java/com/zerava/closetapi/ClosetApiApplication.java
@@ -0,0 +1,13 @@
+package com.zerava.closetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerava/closetapi/ClosetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerava.closetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
